e733a2d09f0060c9a1940ce98a3789f11f32f1d6
Mohsin-Saeed-94 <78295203+Mohsin-Saeed-94@users.noreply.github.com>
2021-07-01 19:14:32 +0500

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'host'),
+        host: env('DATABASE_HOST', 'db-postgresql-nyc3-95945-do-user-8524638-0.b.db.ondigitalocean.com'),
         port: env.int('DATABASE_PORT', '25060'),
-        database: env('DATABASE_NAME', 'db_name'),
-        username: env('DATABASE_USERNAME', 'username'),
-        password: env('DATABASE_PASSWORD', 'password'),
+        database: env('DATABASE_NAME', 'defaultdb'),
+        username: env('DATABASE_USERNAME', 'doadmin'),
+        password: env('DATABASE_PASSWORD', 'tu4xhc3t0yqpf2x1'),
        
         ssl: {
           rejectUnauthorized: false, // For self-signed certificates
