20ef8b7e91adbb2952a49682d47b8adc4176aea8
jui <juioftou@gmail.com>
2021-07-01 17:50:38 +0800

    changed database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,4 @@
-const parse = require('pg-connection-string').parse;
+/* const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = () => ({
@@ -18,6 +18,29 @@ module.exports = () => ({
         },
       },
       options: {},
+    },
+  },
+}); */
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'host'),
+        port: env.int('DATABASE_PORT', port_No),
+        database: env('DATABASE_NAME', 'db_name'),
+        username: env('DATABASE_USERNAME', 'username'),
+        password: env('DATABASE_PASSWORD', 'password'),
+       
+        ssl: {
+          rejectUnauthorized: false, // For self-signed certificates
+        },
+      },
+      options: {
+        ssl: true,
+      },
     },
   },
 });
