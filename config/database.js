b4a3163ba10209203fe6d68d330a23e1a2398dd7
Mohsin-Saeed-94 <78295203+Mohsin-Saeed-94@users.noreply.github.com>
2021-07-01 19:42:33 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,23 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'db-postgresql-nyc3-95945-do-user-8524638-0.b.db.ondigitalocean.com'),
+        port: env.int('DATABASE_PORT', '25060'),
+        database: env('DATABASE_NAME', 'defaultdb'),
+        username: env('DATABASE_USERNAME', 'doadmin'),
+        password: env('DATABASE_PASSWORD', 'tu4xhc3t0yqpf2x1'),
+       
+        ssl: {
+          rejectUnauthorized: false, // For self-signed certificates
+        },
+      },
+      options: {
+        ssl: true,
+      },
+    },
+  },
+});
