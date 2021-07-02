0f14cfd52c1cc85305dee8545f8bb5020b2c49a8
jui <juioftou@gmail.com>
2021-07-02 15:53:31 +0800

    try again

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,3 @@
-// module.exports = ({ env }) => ({
-//     upload: {
-//       provider: "publitio",
-//       providerOptions: {
-//         api_key: env("PUBLITIO_KEY"), //REQUIRED
-//         api_secret: env("PUBLITIO_SECRET"), //REQUIRED
-//         folder: "PROMO APP",
-//         folder_id: "PROMO",
-//       },
-//     },
-//   });
 
 module.exports = ({ env }) => ({
     // ...
