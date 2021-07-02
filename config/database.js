c334d76834efe134a1917ecb4db7b8194b60bd89
jui <juioftou@gmail.com>
2021-07-02 15:17:27 +0800

    okay

--- config/database.js
+++ config/database.js
@@ -21,18 +21,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
