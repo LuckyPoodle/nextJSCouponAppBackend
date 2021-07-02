cfd2e4fda8dfd4a6b6579c9d5198a9807003d614
jui <juioftou@gmail.com>
2021-07-02 18:11:22 +0800

    installed cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,28 @@
+// module.exports = ({ env }) => ({
+//     upload: {
+//       provider: "publitio",
+//       providerOptions: {
+//         api_key: env("PUBLITIO_KEY"), //REQUIRED
+//         api_secret: env("PUBLITIO_SECRET"), //REQUIRED
+//         folder: "PROMO APP",
+//         folder_id: "PROMO",
+//       },
+//     },
+//   });
+
 module.exports = ({ env }) => ({
+    // ...
     upload: {
-      provider: "publitio",
+      provider: 'cloudinary',
       providerOptions: {
-        api_key: env("PUBLITIO_KEY"), //REQUIRED
-        api_secret: env("PUBLITIO_SECRET"), //REQUIRED
-        folder: "PROMO APP",
-        folder_id: "PROMO",
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
+    // ...
   });
