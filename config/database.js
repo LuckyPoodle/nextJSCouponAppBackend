183cf79a38bfec2b90f2db721af4b71ed3739965
jui <juioftou@gmail.com>
2021-07-02 15:03:44 +0800

    readded database.js to config

--- config/database.js
+++ config/database.js
@@ -1,38 +1,38 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         host: env('DATABASE_HOST'),
-//         port: env.int('DATABASE_PORT'),
-//         database: env('DATABASE_NAME'),
-//         username: env('DATABASE_USERNAME'),
-//         password: env('DATABASE_PASSWORD'),
-       
-//         ssl: {
-//           rejectUnauthorized: false, // For self-signed certificates
-//         },
-//       },
-//       options: {
-//         ssl: true,
-//       },
-//     },
-//   },
-// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+       
+        ssl: {
+          rejectUnauthorized: false, // For self-signed certificates
+        },
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
