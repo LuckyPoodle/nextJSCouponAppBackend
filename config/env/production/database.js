762e03a1a1c1ea765034008631748da8c7e14d06
jui <juioftou@gmail.com>
2021-07-02 13:45:03 +0800

    adjusted database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'db-postgresql-sgp1-95962-do-user-9442771-0.b.db.ondigitalocean.com'),
-        port: env.int('DATABASE_PORT', '25060'),
-        database: env('DATABASE_NAME', 'defaultdb'),
-        username: env('DATABASE_USERNAME', 'doadmin'),
-        password: env('DATABASE_PASSWORD', 'l9fpixk7khnc1hiz'),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
        
         ssl: {
           rejectUnauthorized: false, // For self-signed certificates
