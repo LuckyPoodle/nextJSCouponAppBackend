0b79bdcd9afa01bceb63f553d2b1418e72a8ac46
truffletou <juioftou@gmail.com>
2021-07-15 18:24:49 +0800

    digitaloceanspace

--- config/plugins.js
+++ config/plugins.js
@@ -10,19 +10,35 @@
 //     },
 //   });
 
+
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        delete: {},
-      },
-    },
-    // ...
-  });
+  upload: {
+    provider: "do",
+    providerOptions: {
+      key: process.env.DO_SPACE_ACCESS_KEY,
+      secret: process.env.DO_SPACE_SECRET_KEY,
+      endpoint: process.env.DO_SPACE_ENDPOINT,
+      space: process.env.DO_SPACE_BUCKET,
+      directory: process.env.DO_SPACE_DIRECTORY,
+      cdn: process.env.DO_SPACE_CDN,
+    }
+  },
+});
+
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//       provider: 'cloudinary',
+//       providerOptions: {
+//         cloud_name: env('CLOUDINARY_NAME'),
+//         api_key: env('CLOUDINARY_KEY'),
+//         api_secret: env('CLOUDINARY_SECRET'),
+//       },
+//       actionOptions: {
+//         upload: {},
+//         delete: {},
+//       },
+//     },
+//     // ...
+//   });
+
