12940f94480802927987005fcf983f4b7b5c5360
truffletou <juioftou@gmail.com>
2021-07-18 23:12:04 +0800

    added urlname field

--- config/database.js
+++ config/database.js
@@ -1,22 +1,37 @@
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'postgres',
+//         host: env('DATABASE_HOST'),
+//         port: env.int('DATABASE_PORT'),
+//         database: env('DATABASE_NAME'),
+//         username: env('DATABASE_USERNAME'),
+//         password: env('DATABASE_PASSWORD'),
+       
+//         ssl: {
+//           rejectUnauthorized: false, // For self-signed certificates
+//         },
+//       },
+//       options: {
+//         ssl: true,
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-       
-        ssl: {
-          rejectUnauthorized: false, // For self-signed certificates
-        },
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: true,
+        useNullAsDefault: true,
       },
     },
   },
